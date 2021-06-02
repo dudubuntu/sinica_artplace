a612ded26a278764a6a20a45d7cb83110b89ae74
Денис <piligrim021@gmail.com>
2021-06-02 20:04:38 +0300

    delete non-actual files

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Item
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = ['articul', 'name', 'description', 'price', 'action', 'previous_price', 'event_date', 'img']
--- web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-
-app_name = 'web'
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-]
