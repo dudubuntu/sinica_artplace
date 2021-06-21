e9fe0d12120c1efdf6550d8eb4b225de64853275
Денис <piligrim021@gmail.com>
2021-06-21 17:07:23 +0300

    entrypoint.sh

--- Docker/sinica_artplace/entrypoint.sh
+++ Docker/sinica_artplace/entrypoint.sh
@@ -4,6 +4,8 @@ python manage.py makemigrations --no-input
 
 python manage.py makemigrations api --no-input
 
+python manage.py makemigrations contactus --no-input
+
 python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
