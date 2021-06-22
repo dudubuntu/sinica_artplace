c88f586ead43535904f9483ed921ffc45d7ab64c
Денис <piligrim021@gmail.com>
2021-06-23 00:44:18 +0300

    api serializer fix

--- Docker/sinica_artplace/api/serializers.py
+++ Docker/sinica_artplace/api/serializers.py
@@ -6,4 +6,4 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['articul', 'name', 'author', 'poster', 'author_ing', 'description', 'price', 'action', 'previous_price', 'event_date']
+        fields = ['articul', 'name', 'author', 'poster', 'author_img', 'description', 'price', 'action', 'previous_price', 'event_date']
